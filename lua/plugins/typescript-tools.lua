90dff292a99ed3a8932a01997e9a8c0ef936537f
Nicolas Ramirez <nramirez@theknotww.com>
2025-09-17 06:36:54 +0200

    ts

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -6,6 +6,7 @@ return {
     ts.setup {
       settings = {
         tsserver_file_preferences = {
+          importModuleSpecifier = 'relative',
           importModuleSpecifierPreference = 'relative',
           importModuleSpecifierEnding = 'minimal',
         },
